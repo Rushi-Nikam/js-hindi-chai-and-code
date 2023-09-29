876330cd3729f6f74f89dd35e5291d6fa98162ad
Rushi-Nikam <122095441+Rushi-Nikam@users.noreply.github.com>
2023-09-29 06:16:24 +0000

    understanding the variables

---
+++ #01_basic/01_veriable.js
@@ -0,0 +1,6 @@
+const accountId=12;
+let accountEmail="rushidnikam00@gmail.com";
+var accountPassword="12345";
+accoutCity="jaipur";
+//accountId=2;  // NOT ALLOWED
+console.log(accountId);
--- #01_basic/test.js
+++ #01_basic/test.js
@@ -1,1 +1,1 @@
-console.log("rushi")
+console.log("rushi");
