a8845bd7a10aa217b229946c822e754be137602e
Rushi-Nikam <122095441+Rushi-Nikam@users.noreply.github.com>
2023-09-29 07:00:00 +0000

    Datatypes:primitive vs non-primitive

---
+++ #01_basic/02_DataType.js
@@ -0,0 +1,17 @@
+"use strict";
+// alert("hello");//we are using nodejs not browser
+console.log(3 + 3);
+//maintain this practice for code readibility ;
+console.log("Rushikesh");
+let name = "rushi";
+// primitive datatype
+//number =>2 to power 53
+//bigint
+// string => " ",'',``;
+// boolean => true/flase
+//null => standalone vlaue
+//undefined=> that value is not define
+//symbol =>
+
+//non-primitive
+//object
