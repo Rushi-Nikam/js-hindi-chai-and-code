abb8dd0f758f6eff84ad50079a37e7702ba00847
Rushi-Nikam <122095441+Rushi-Nikam@users.noreply.github.com>
2023-09-29 06:36:37 +0000

    not using var

--- #01_basic/01_veriable.js
+++ #01_basic/01_veriable.js
@@ -1,7 +1,8 @@
 const accountId=12;
 let accountEmail="rushidnikam00@gmail.com";
-var accountPassword="12345";
+var accountPassword="12345"; /*prefer not to use because of issue in block scope and functional scope */
 accoutCity="jaipur";
+console.log(typeof(accoutCity));
 //accountId=2;  // NOT ALLOWED
 console.log(accountId);
 console.table([accountEmail,accountId,accountPassword,accoutCity]);
