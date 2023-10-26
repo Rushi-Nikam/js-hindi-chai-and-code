fd6194dbe92d4dc99cde12419131f3f4b4f529c0
Rushi-Nikam <122095441+Rushi-Nikam@users.noreply.github.com>
2023-10-26 17:35:22 +0000

    operations: how they works

---
+++ #01_basic/03conversion&operations.js
@@ -0,0 +1,51 @@
+let score="SHUBHAGHI" //33//NUMBER
+// console.log(typeof score);
+// console.log(typeof(score));
+//let score="SHUBHAGHI" //33//NUMBER
+//let score="SHUBHAGHI" //33//NUMBER
+ ///33ABC//NAN
+ //STRING // NAN
+ //true=>1 //flase=>0
+
+let valueInNumber= Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+let isLoggedIn="rushi";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+//1=>true 0=>flase // ""=> flase ,"rushi"=>true
+let someNumber =33;
+let stringNumber =String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+//************************************operations***********************************/
+let value=3;
+let negvalue = -value;
+// console.log(negvalue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2^2);
+// console.log(2%2);
+// console.log(2**2);
+let str1= "hello";
+let str2= " Rushikesh";
+let str3= str1 + str2;
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2+2);
+// console.log(1 + 2+"2");
+// console.log((2+3) * 4/5%2);
+// console.log(true);
+// console.log(+true);
+// console.log(true+) ; //not possible
+//console.log(+"") ; //not possible
+let num1 , num2, num3;
+num1=num2 =num3 = 2+2;
+let gamecounter =100;
+gamecounter++;
+++gamecounter;
+console.log(gamecounter);
