18f3dfa5d9d31140631394a7fba35e3654cf623a
Rushi-Nikam <122095441+Rushi-Nikam@users.noreply.github.com>
2023-10-26 17:13:18 +0000

    conversions :how to work the conversions

---
+++ #01_basic/conversion&operations.js
@@ -0,0 +1,20 @@
+let score="SHUBHAGHI" //33//NUMBER
+// console.log(typeof score);
+// console.log(typeof(score));
+//let score="SHUBHAGHI" //33//NUMBER
+//let score="SHUBHAGHI" //33//NUMBER
+ ///33ABC//NAN
+ //STRING // NAN
+ //true=>1 //flase=>0
+
+let valueInNumber= Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+let isLoggedIn="rushi";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+//1=>true 0=>flase // ""=> flase ,"rushi"=>true
+let someNumber =33;
+let stringNumber =String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
