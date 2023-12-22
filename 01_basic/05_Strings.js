0cb9328795bc1a86dfabc3c270a54fc9fa5e3bb2
Rushi-Nikam <122095441+Rushi-Nikam@users.noreply.github.com>
2023-12-22 17:42:26 +0000

    String structure

---
+++ 01_basic/05_Strings.js
@@ -0,0 +1,6 @@
+let name ="rushi"
+let count = 50
+console.log(typeof name);
+console.log(typeof String);
+console.log(`The name of the student is ${name} and marks is ${count}`)
+let anotherobj = new String("rushikesh Nikam");
