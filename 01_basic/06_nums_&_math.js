906caff0d26d1c9378e5ab68db8f236e5078c9bf
Rushi-Nikam <122095441+Rushi-Nikam@users.noreply.github.com>
2023-12-24 12:27:07 +0000

    Numbers properties and Math properties

---
+++ 01_basic/06_nums_&_math.js
@@ -0,0 +1,24 @@
+const score = 400;
+console.log(score);
+const balance = new Number(100);
+// console.log(balance);
+// console.log(balance.toString())
+// console.log(balance.toString().length)
+const otherNumber = 123.8955;
+// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toExponential(3))
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString("en-IN"));
+
+//+++++++++++++++++++++++++Maths+++++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4)) //positive always positive and negative convert to positive
+// console.log(Math.round(5.6));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.5));
+// console.log(Math.sqrt(16));
+// console.log(Math.min(16,4,5,2,8));
+// console.log(Math.max(16,4,5,2,8));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10));
