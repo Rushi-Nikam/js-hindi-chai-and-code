c9e1f81d93cab0064490e26659c64b7f987c08f9
Rushi-Nikam <122095441+Rushi-Nikam@users.noreply.github.com>
2024-01-19 15:43:03 +0000

    scope explained

---
+++ 03_basic/02_scope.js
@@ -0,0 +1,12 @@
+// var c = 40;
+var data = 40;
+
+{
+    let data = 100;
+    let data1 = 200;
+    let data2 = 300;
+    console.log("inner the scope :" + data);
+
+}
+console.log(data);
+// console.log(data1);
