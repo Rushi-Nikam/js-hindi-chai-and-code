074444836892f694f25cb1b8ba960ce1dfb8a08c
Rushi-Nikam <122095441+Rushi-Nikam@users.noreply.github.com>
2024-01-22 05:09:25 +0000

    function hoisting

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -14,5 +14,32 @@ function one(){
     const username = "rushikesh";
     function two(){
         const website= "youtube";
+        console.log(website)
+        console.log(username)
     }
+    // console.log(website)
+    // two();
 }
+// one();
+
+if(true){
+    const username = "rushikesh";
+    if(username === "rushikesh"){
+        const website = " Youtube";
+        console.log(username + website);
+      
+    }
+    // console.log(website); error
+    
+}
+// console.log(username); error
+
+console.log(addone(5))  // we don't face a problem 
+function addone(num){
+  return num + 1;
+}
+// addTwo(5);          // we face a prblem 
+const addTwo = function(num){
+    return num + 2;
+}
+addTwo(5)
