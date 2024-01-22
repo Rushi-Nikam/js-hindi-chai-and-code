527eafa10e9b4a34fead7691f323e36d43012653
Rushi-Nikam <122095441+Rushi-Nikam@users.noreply.github.com>
2024-01-22 10:51:49 +0000

    switch case understanding

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,71 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+let month = 10;
+
+switch (month) {
+    case 1:
+        console.log('january');
+        break;
+    case 2:
+        console.log('feb');
+        break;
+    case 3:
+        console.log('March');
+        break;
+    case 4:
+        console.log('April');
+        break;
+    case 5:
+        console.log('may');
+        break;
+    case 6:
+        console.log('june');
+        break;
+    case 7:
+        console.log('july');
+        break;
+    case 8:
+        console.log('August');
+        break;
+    case 9:
+        console.log('september');
+        break;
+    case 10:
+        console.log('octomber');
+        break;
+    case 11:
+        console.log('November');
+        break;
+    case 12:
+        console.log('December');
+        break;
+
+    default: 
+    console.log('no result match')
+        break;
+}
+let color= 'orange';
+switch (color) {
+    case 'red':
+        console.log('user is confident')
+        break;
+    case 'blue':
+        console.log('user is piecefull')
+        break;
+    case 'yellow':
+        console.log('user is shy')
+        break;
+    case 'orange':
+        console.log('user is motivated')
+        break;
+
+    default: 
+    console.log('no one match');
+        break;
+}
