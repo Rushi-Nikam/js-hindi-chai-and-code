06152f3ce36bcf1a1996e224bf668363bf0e932b
Rushi-Nikam <122095441+Rushi-Nikam@users.noreply.github.com>
2024-01-31 14:24:49 +0000

    project1 js code

--- 07_projects/Project1/Project1.js
+++ 07_projects/Project1/Project1.js
@@ -1,61 +1,19 @@
-* {
-  box-sizing: border-box;
-}
 
-body {
-  margin: 0;
-  font-family: system-ui, sans-serif;
-  color: black;
-  background-color: white;
-}
-
-nav {
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
-  padding: 0.5rem;
-  gap: 0.5rem;
-  border-bottom: solid 1px #aaa;
-  background-color: #eee;
-}
-
-nav a {
-  display: inline-block;
-  min-width: 9rem;
-  padding: 0.5rem;
-  border-radius: 0.2rem;
-  border: solid 1px rgb(22, 22, 22);
-  text-align: center;
-  text-decoration: none;
-  color: #1b1b1b;
-}
-
-nav a[aria-current='page'] {
-  color: #000;
-  background-color: #d4d4d4;
-}
-
-main {
-  padding: 1rem;
-}
-
-h1 {
-  font-weight: bold;
-  font-size: 1.5rem;
-}
-
-.projects {
-  display: flex;
-  flex-direction: column;
-}
-
-.project-link {
-  background-color: #fff;
-  padding: 10px 30px;
-  border-radius: 8px;
-  color: #212121;
-  text-decoration: none;
-  border: 2px solid #212121;
-  margin-top: 5px;
-}
+const Button = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+// console.log(Button);
+Button.forEach((button) => {
+  //  console.log(button);
+  button.addEventListener('click', (event) => {
+    console.log(event.target.id);
+    if (event.target.id === 'grey') {
+      body.style.backgroundColor = event.target.id;
+    } else if (event.target.id === 'white') {
+      body.style.backgroundColor = event.target.id;
+    } else if (event.target.id === 'blue') {
+      body.style.backgroundColor = event.target.id;
+    } else if (event.target.id === 'yellow') {
+      body.style.backgroundColor = event.target.id;
+    }
+  });
+});
