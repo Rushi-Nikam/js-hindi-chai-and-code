be6c1087c1aa4aa6968bdada38865728b7e818d7
Rushi-Nikam <122095441+Rushi-Nikam@users.noreply.github.com>
2024-02-05 12:40:34 +0000

    project6 upload

---
+++ 07_projects/Project6/Project6.js
@@ -0,0 +1,27 @@
+const ramdomColor = function(){
+    const hex = '0123456ABCDEF';
+    let Color = "#";
+    for(let i=0;i< 6;i++ ){
+     Color += hex[Math.floor(Math.random() * 16)]
+    
+    }
+    return Color;
+  };
+  let changingcolor;
+  const StartChaning = function(){
+    if(!changingcolor){
+      changingcolor =  setInterval(changeBgColor,1000)
+    }
+    function changeBgColor(){
+      
+      document.body.style.backgroundColor = ramdomColor();
+    }
+    
+  }
+  const stopChaning= function(){
+    clearInterval(changingcolor)
+    changingcolor = null;
+  }
+  document.querySelector('#start').addEventListener('click',StartChaning);
+  document.querySelector('#stop').addEventListener('click',stopChaning);
+  
